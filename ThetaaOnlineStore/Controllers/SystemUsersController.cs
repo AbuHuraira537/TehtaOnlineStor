9776ae424e92ebc1b4fe80d8e9fa8540228e4ab3
AbuHuraira537 <abouhuraira967@gmail.com>
2020-05-12 16:15:51 +0500

    SystemUser controller is completed with register flow

---
+++ ThetaaOnlineStore/Controllers/SystemUsersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ThetaaOnlineStore.Models;
+
+namespace ThetaaOnlineStore.Controllers
+{
+   
+    public class SystemUsersController : Controller
+    {
+        ThetaOnlineStoreContext ORM = null;
+        public SystemUsersController(ThetaOnlineStoreContext orm)
+        {
+            ORM = orm;
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register([Bind("UserName", "Password", "Email", "CreatedBy", "Role", "Status", "Mobile")] SystemUser sm)
+        {
+            if(ModelState.IsValid)
+            {
+                ORM.SystemUser.Add(sm);
+                await ORM.SaveChangesAsync();
+            }
+
+            return View();
+        }
+    }
+}
--- ThetaaOnlineStore/Startup.cs
+++ ThetaaOnlineStore/Startup.cs
@@ -66,7 +66,7 @@ namespace ThetaaOnlineStore
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=SystemUsers}/{action=Register}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
