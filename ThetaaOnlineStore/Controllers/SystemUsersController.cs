90fbce3a21dfdec0d51a057ce8dd2971be6fe9cd
AbuHuraira537 <abouhuraira967@gmail.com>
2020-05-13 11:46:03 +0500

    Register completed. and CURD with search completed

--- ThetaaOnlineStore/Controllers/SystemUsersController.cs
+++ ThetaaOnlineStore/Controllers/SystemUsersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ThetaaOnlineStore.Models;
 
 namespace ThetaaOnlineStore.Controllers
@@ -23,15 +24,92 @@ namespace ThetaaOnlineStore.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Register([Bind("UserName", "Password", "Email", "CreatedBy", "Role", "Status", "Mobile")] SystemUser sm)
+        public async Task<IActionResult> Register([Bind("UserName", "Password", "DisplayName", "Email", "Mobile", "Status", "Role", "CreatedDate", "CreatedBy")] SystemUser sm)
         {
             if(ModelState.IsValid)
             {
                 ORM.SystemUser.Add(sm);
                 await ORM.SaveChangesAsync();
+                ViewBag.Message = "User added sucessfully!";
+            }
+            else
+            {
+                ViewBag.Message = "Not added sucessfully check fields!";
             }
 
+
             return View();
+        }
+        public IActionResult ShowAll(string searchquery)
+        {
+            if(searchquery!=null)
+            {
+                return View(ORM.SystemUser.Where(a=>a.UserName.Contains(searchquery)).ToList<SystemUser>());
+
+            }
+            else
+            {
+                return View(ORM.SystemUser.ToList<SystemUser>());
+            }
+
+        }
+        public async Task<IActionResult> Detail(int id)
+        {
+            SystemUser user=await ORM.SystemUser.FindAsync(id);
+            if(user==null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(user);
+            }    
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            SystemUser user = await ORM.SystemUser.FindAsync(id);
+            if(user!=null)
+            {
+                return View(user);
+            }
+            else
+            {
+                return NotFound();
+            }
+            
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task< IActionResult> Edit(SystemUser user)
+        {
+            
+            if(user!=null)
+            {
+
+                ORM.Update(user);
+               await ORM.SaveChangesAsync();
+                return RedirectToAction(nameof(Register));
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            SystemUser s = await ORM.SystemUser.FindAsync(id);
+            if (s != null)
+            {
+                ORM.SystemUser.Remove(s);
+                await ORM.SaveChangesAsync();
+                return RedirectToAction(nameof(ShowAll));
+            }
+            else
+            {
+                return NotFound();
+            }
+
         }
     }
 }
